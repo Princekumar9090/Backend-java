512309fda005d997e82c7acfb2d0602417246342
Princekumar9090 <109418974+Princekumar9090@users.noreply.github.com>
2022-08-02 20:41:30 +0530

    Add Crud APIs
    Add Crud APIs

---
+++ frontend-testing/src/main/java/com/example/frontendtesting/FrontendTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontendtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendTestingApplication.class, args);
+	}
+
+}
---
+++ frontend-testing/src/test/java/com/example/frontendtesting/FrontendTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontendtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
